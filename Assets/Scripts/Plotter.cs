f82ecc07b2d3b230759a06e4162ca384dd68f629
pavan <bestpavan@gmail.com>
2020-12-25 17:41:39 +1100

    added superposition

--- Assets/Scripts/Plotter.cs
+++ Assets/Scripts/Plotter.cs
@@ -25,26 +25,37 @@ public class Plotter : MonoBehaviour
             }
             objects.Clear();
 
+            float[] nArray = new float[] { 1,2 };
             float numberOfPoints = 200;
-            float n = 10;
+            Complex[] cumulitavePsi = new Complex[(int)numberOfPoints];
             float a = 4;
-            float x = 0;
             float hBar = 0.001f;
             float m = 1;
+            foreach (var n in nArray)
+            {
+                float x = 0;
+                for (int i = 0; i < numberOfPoints; i++)
+                {
+                    Complex psi = Mathf.Sqrt(2 / a) * Mathf.Sin(n * (Mathf.PI / a) * x) * Complex.Exp((-Complex.ImaginaryOne * Mathf.Pow(n, 2) * Mathf.Pow(Mathf.PI, 2) * hBar) / (2 * m * Mathf.Pow(a, 2)) * t);
+                    cumulitavePsi[i] += psi;
+                    x += a / numberOfPoints;
+                }
+            }
+
+            float x2 = 0;
             for (int i = 0; i < numberOfPoints; i++)
             {
-                Complex psi = Mathf.Sqrt(2/a) * Mathf.Sin(n * (Mathf.PI/a) * x) * Complex.Exp((-Complex.ImaginaryOne * Mathf.Pow(n,2) * Mathf.Pow(Mathf.PI, 2) * hBar)/(2 * m * Mathf.Pow(a, 2)) * t);
                 var amplitudeClone = Instantiate(GameObject.Find("Sphere"));
                 GameObject.Find("Sphere").GetComponent<Renderer>().material.color = Color.cyan;
                 objects.Add(amplitudeClone.gameObject);
-                amplitudeClone.transform.localPosition = new UnityEngine.Vector3(x, (float)psi.Real, (float)psi.Imaginary);
+                amplitudeClone.transform.localPosition = new UnityEngine.Vector3(x2, (float)cumulitavePsi[i].Real, (float)cumulitavePsi[i].Imaginary);
 
                 var probabilityClone = Instantiate(GameObject.Find("Sphere2"));
                 objects.Add(probabilityClone.gameObject);
-                probabilityClone.transform.localPosition = new UnityEngine.Vector3(x, Mathf.Pow((float)psi.Magnitude,2));
+                probabilityClone.transform.localPosition = new UnityEngine.Vector3(x2, Mathf.Pow((float)cumulitavePsi[i].Magnitude, 2));
                 GameObject.Find("Sphere2").GetComponent<Renderer>().material.color = Color.black;
 
-                x += a / numberOfPoints;
+                x2 += a / numberOfPoints;
             }
             t += 10;
 
