21735f99a9fe8f46b8788f5eb4da456ec5a5374f
pavan <bestpavan@gmail.com>
2020-12-26 11:18:03 +1100

    mouse scroll

--- Assets/Scripts/MouseScript.cs
+++ Assets/Scripts/MouseScript.cs
@@ -6,6 +6,7 @@ public class MouseScript : MonoBehaviour
 {
     float speed = 5.0f;
     float sensitivity = 5f;
+    float scrollSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,15 @@ public class MouseScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
+        {
+            transform.Translate(0, 0, 1 * scrollSpeed, Space.Self);
+        }
+        else if (d < 0f)
+        {
+            transform.Translate(0, 0, -1 * scrollSpeed, Space.Self);
+        }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
@@ -23,6 +33,14 @@ public class MouseScript : MonoBehaviour
         else if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
 
         if (Input.GetMouseButton(0))
